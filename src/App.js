88b51ff6192bb2913f5a10d6d79dbcc7e38d06a9
Ling Igor <lingigor1990@gmail.com>
2023-09-10 00:24:22 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-// import { useState } from 'react';
-// import LoginForm from './components/Signin/Signin';
-// import { Route, Routes } from 'react-router-dom';
-// import { Layout } from './components/Layout/Layout';
-// import { Home } from './components/Home/Home';
-// import Register from './components/Signup/Signup';
-// import { OneProduct } from './components/OneProduct/OneProduct';
+import { useState } from 'react';
+import LoginForm from './components/Signin/Signin';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { Home } from './components/Home/Home';
+import Register from './components/Signup/Signup';
+import { OneProduct } from './components/OneProduct/OneProduct';
 
 function App() {
-  // const [login, Setlogin] = useState('');
-  // // eslint-disable-next-line
-  // const [showOverlay, SetshowOverlay] = useState(false);
+  const [login, Setlogin] = useState('');
+  // eslint-disable-next-line
+  const [showOverlay, SetshowOverlay] = useState(false);
 
-  // const handleLogin = () => {
-  //   Setlogin(true);
-  //   SetshowOverlay(true);
-  // };
+  const handleLogin = () => {
+    Setlogin(true);
+    SetshowOverlay(true);
+  };
 
-  // const closeForm = () => {
-  //   Setlogin(false);
-  //   SetshowOverlay(false);
-  // };
+  const closeForm = () => {
+    Setlogin(false);
+    SetshowOverlay(false);
+  };
 
   return (
     <div
       style={{ paddingBottom: '40px', maxWidth: '1400px', margin: '0 auto' }}
     >
-      {' '}
-      Hello
-      {/* {login ? (
+      {login ? (
         <LoginForm closeForm={closeForm} backdrop={SetshowOverlay} />
       ) : (
         <></>
       )}
       <Routes>
-        <Route path="/" element={<Layout handleLogin={handleLogin}/>}>
+        <Route path="/" element={<Layout handleLogin={handleLogin} />}>
           <Route index element={<Home />} />
-          <Route path="/product/:id" element={<OneProduct/>}/>
+          <Route path="/product/:id" element={<OneProduct />} />
           <Route path="/register" element={<Register />} />
         </Route>
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
